22417c83bca31a822f11ce2c60964f5b54608b28
mke-maker <102038989+mke-maker@users.noreply.github.com>
2024-01-29 19:55:38 -0600

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,32 @@
+include //whatever
+#define dac_addr
+#def display_addr
+#def color_addr
+
+
+//recode all this as a state machine
+
+
+init();
+
+//following should be interrup driven I suppose.
+
+
+if pushbutton pressed //need debounce
+random=get_random();
+switch selection //need debounce/interrupts here
+case 0;
+color(random);
+case 1
+stratagy(random);
+case 2
+while (pushbutton)
+  tune(random);
+case 3
+  article(random);
+case 4
+  weather(random);
+case 6
+  number(random);
+case 7
+  testmode();  //use a pot on the adc to control each of the interfaces, selected with PB.  
